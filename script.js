d50e12fc2ae5643a96e57bc8da3b47542098279b
ajbates2 <60674015+ajbates2@users.noreply.github.com>
2020-04-04 21:02:12 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const apiKey = ''; 
+const searchURL = '';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.items.length; i++){
+    $('#results-list').append(
+      `<li><h3>${responseJson.items[i].snippet.title}</h3>
+      <p>${responseJson.items[i].snippet.description}</p>
+      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      </li>`
+    )}; 
+  $('#results').removeClass('hidden');
+};
+
+function getResource(query, maxResults=10) {
+  const params = {
+    key: apiKey,
+    q: query,
+    part: 'snippet',
+    maxResults,
+    type: 'video'
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getResource(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
